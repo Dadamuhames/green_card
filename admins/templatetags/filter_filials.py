b02a1169a0a2a8db641dde38e15a86d63f7fa9f9
MSD <dadamuhames@gmail.com>
2023-03-13 14:37:01 +0500

    Fix

--- admins/templatetags/filter_filials.py
+++ admins/templatetags/filter_filials.py
@@ -3,8 +3,6 @@ from django.template.defaulttags import register
 
 @register.filter
 def filter_filial(queryset, status):
-    print('!!!', queryset)
-
     if queryset is None:
         return 0
     return queryset.filter(status=status).count()
